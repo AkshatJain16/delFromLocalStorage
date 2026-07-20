c9bc8bdd2f3de0b73a7d0d0c08be94660a87481c
AkshatJain16 <akshatjain0223@gmail.com>
2026-07-20 13:20:51 +0530

    Created form and handled submit

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-const mainHeading = document.querySelector('#main-heading');
-mainHeading .style.textAlign = 'right';
-mainHeading .style.marginRight = '30px';
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    const username = event.target.username.value; // Get the username from the form input
+    const email = event.target.email.value; // Get the email from the form input
+    const phone = event.target.phone.value; // Get the phone number from the form input
 
-const header = document.querySelector('#header');
-header.style.padding = '20px';
-header.style.borderTopLeftRadius = '10px';
-header.style.borderTopRightRadius = '10px';
-
-const basketHeading = document.querySelector('#basket-heading');
-basketHeading.style.color = 'brown';
-basketHeading.style.marginLeft = '30px';
-
-const fruits = document.querySelector(".fruits");
-fruits.style.listStyleType = "none";
-fruits.style.padding = "30px";
-fruits.style.backgroundColor= 'gray';
-fruits.style.marginLeft= '30px';
-fruits.style.marginRight= '30px';
-fruits.style.borderRadius='5px';
-
-const fruit = document.querySelectorAll(".fruit");
-for (let i = 0; i < fruit.length; i++){
-    if (i % 2 != 0) {
-        fruit[i].style.backgroundColor = 'brown';
-        fruit[i].style.color = 'white';
-        
-    }else{
-        fruit[i].style.backgroundColor = 'lightgray';
-        fruit[i].style.color = 'black';
-    }
-    fruit[i].style.margin= '5px';
-    fruit[i].style.padding= '10px';
-    fruit[i].style.borderRadius= '5px';
+    localStorage.setItem('username', username); // Store the username in local storage
+    localStorage.setItem('email', email); // Store the email in local storage
+    localStorage.setItem('phone', phone); // Store the phone number in local storage
 }
